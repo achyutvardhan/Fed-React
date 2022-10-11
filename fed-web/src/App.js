bc1f039c860c315686d7cd9638d577a775e9244a
achyut vardhan <achyutvardhan234@gmail.com>
2022-10-11 17:33:10 +0530

    created react file

---
+++ fed-web/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import NavBar from "./component/NavBar";
+import Footer from "./component/Footer";
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
