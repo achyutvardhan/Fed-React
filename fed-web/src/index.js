2e8890e4750af1af67dac811ebb5065c1dc1f8b0
achyut vardhan <achyutvardhan234@gmail.com>
2022-10-12 01:41:49 +0530

    added nav bar

--- fed-web/src/index.js
+++ fed-web/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './component/css/nav.css'
+import './component/js/nav'
+import './component/js/navMenu'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
