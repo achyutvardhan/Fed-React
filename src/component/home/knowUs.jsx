d5cea58680ecebaa3104754c64d77435f0638af8
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-10-15 02:39:11 +0530

    Update knowUs.jsx

--- src/component/home/knowUs.jsx
+++ src/component/home/knowUs.jsx
@@ -7,35 +7,35 @@ import how from "../../Img/how.png";
 import what from "../../Img/what.png";
 
 export default function knowUs() {
-  
   const knowus = [
     {
-      class : 'whyDiv',
-      img : `${why}`,
-      for : 'Why?',
-      content1 : 'We aim to nurture entrepreneurship through',
-      content2 : 'creative, authentic, and efficient techniques.',
-      content3 : ''
-
+      class: "whyDiv",
+      img: `${why}`,
+      for: "Why?",
+      content1: "We aim to nurture entrepreneurship through",
+      content2: "creative, authentic, and efficient techniques.",
+      content3: "",
     },
     {
-      class : 'HowDiv',
-      img : `${how}`,
-      for : 'how?',
-      content1 : 'Strong co-operation between members that our organization instills is how we can accomplish all of this.',
-      content2 : ' There is no I, only we. We exist as a team, function as a team,',
-      content3 : 'and execute as a team.'
+      class: "HowDiv",
+      img: `${how}`,
+      for: "How?",
+      content1:
+        "Strong co-operation between members that our organization instills is how we can accomplish all of this.",
+      content2:
+        " There is no I, only we. We exist as a team, function as a team,",
+      content3: "and execute as a team.",
     },
     {
-      class : 'WhatDiv',
-      img : `${what}`,
-      for : 'What?',
-      content1 : 'Through our social media handles, we',
-      content2 : ' arrange informative podcasts, high-quality events, and inspiring and thought-provoking material.',
-      content3 : ''
-
-    }
-  ]
+      class: "WhatDiv",
+      img: `${what}`,
+      for: "What?",
+      content1: "Through our social media handles, we",
+      content2:
+        " arrange informative podcasts, high-quality events, and inspiring and thought-provoking material.",
+      content3: "",
+    },
+  ];
 
   return (
     <>
@@ -57,12 +57,12 @@ export default function knowUs() {
         </div>
         {/* Why_What_How */}
         <div className="WhyHowWhatDiv">
-        <Content knowus = {knowus[0]}/>
-          <Content knowus = {knowus[1]}/>
-          <Content knowus = {knowus[2]}/>
+          <Content knowus={knowus[0]} />
+          <Content knowus={knowus[1]} />
+          <Content knowus={knowus[2]} />
         </div>
         {/* Believe */}
-        <Believer/>
+        <Believer />
       </section>
     </>
   );
