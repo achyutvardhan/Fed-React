4b4d307c8e4f9415ab62f073d03442a8666d0bc8
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-10-14 22:02:28 +0530

    cleaning up

---
+++ src/App.js
@@ -0,0 +1,23 @@
+// import "./App.css";
+import NavBar from "./component/NavBar";
+import Footer from "./component/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/index";
+
+function App() {
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/sign-up" element={<SignUp />} /> */}
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
