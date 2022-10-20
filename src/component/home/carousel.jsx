fcb0a5811a82014a9d55dd48861911a4d650bae1
achyut vardhan <achyutvardhan234@gmail.com>
2022-10-21 00:44:08 +0530

    completed carousel

--- src/component/home/carousel.jsx
+++ src/component/home/carousel.jsx
@@ -1,41 +1,38 @@
-
-
-
+// Import Swiper styles
+import { Navigation, Pagination, A11y, Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "../carousel/carou.css";
+import img1 from "../../Img/header/pexels-anastasia-shuraeva-7278606.jpg";
+import img2 from "../../Img/header/h1.png";
+import img3 from "../../Img/header/Maskgroup.png";
 
 // Import Swiper styles
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-import desktop from "../../Img/Desktop.png"
-import desktop1 from "../../Img/Desktop.png"
-import desktop2 from "../../Img/Desktop.png"
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import 'swiper/css/autoplay'
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
 
 export default () => {
-   
   return (
     <Swiper
       // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
+      modules={[Navigation, Pagination, A11y, Autoplay]}
       slidesPerView={1}
       navigation
       pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
       onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
+      onSlideChange={() => console.log("slide change")}
+      autoplay={{ delay: 6000 }}
     >
-       
-      <SwiperSlide><img src={desktop} alt="" /></SwiperSlide>
-      <SwiperSlide><img src={desktop} alt="" /></SwiperSlide>
-      <SwiperSlide><img src={desktop} alt="" /></SwiperSlide>
+      <SwiperSlide>
+        <img src={img1} className="carousel" alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={img2} className="carousel" alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={img3} className="carousel" alt="" />
+      </SwiperSlide>
     </Swiper>
   );
 };
